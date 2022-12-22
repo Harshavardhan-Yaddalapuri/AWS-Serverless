609420706e41cc605aa3a1380935d437c3ac923e
harshavardhan <haarshavardhan08@gmail.com>
2022-12-23 02:13:45 +0530

    changed cognito poolid, appclientid, region in js/config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_UADsUTgRB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2ufi72a5jran220hpnk8glcs6k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_xoLPCD2bA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1rbl5mo4mfak0i5954ra9afkeq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
